20e763dc4350b3096b6c29417495c703d42f3192
shreyapatil1047-lab <shreyapatil1047@gmail.com>
2026-03-23 20:14:22 +0530

    Updated app title to Shreya App

--- src/components/AppName.jsx
+++ src/components/AppName.jsx
@@ -1,7 +1,12 @@
 import styles from "./AppName.module.css";
 
 function AppName() {
-  return <h1 className={styles.todoHeading}>TODO App</h1>;
+  return (
+    <h1 className={styles.todoHeading}>
+      Shreya's App<br></br>
+      Have a nice day!
+    </h1>
+  );
 }
 
 export default AppName;
